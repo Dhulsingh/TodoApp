79a2e3f5922c97ec222fa02be24052ddc1fc3eb6
dhulsingh12 <dhulsingh1@gmail.com>
2023-02-17 04:08:48 +0000

    TodoApp

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import FormTodo from './components/FormTodo'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>TODO app</h1>
+      <FormTodo />
+    </div>
+  )
+}
+
+export default App
